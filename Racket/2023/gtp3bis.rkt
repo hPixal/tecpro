1e628c699605d28ddacf902a3414ad41cad161ab
hPixal <nahuel.j.gareis@protonmail.com>
2023-04-06 12:52:53 +0000

    masguias

--- Racket/2023/gtp3bis.rkt
+++ Racket/2023/gtp3bis.rkt
@@ -73,16 +73,113 @@
                               )
                           ))
 
+(define miniLista-r(lambda (x ls rtn)
+    (if (null? ls)
+        rtn
+        (define ((first (car ls)) (rest (cdr ls)))
+            (if (equal? first x)
+                (miniLista-r x rest (cons first rtn))
+                (miniLista-r x rest rtn)
+            )
 
-(define agrupar-r2 (lambda (ls)))
+        )
+    )
+))
 
-(define agrupar-r1 (lambda (ls aux)
-    
+(define miniLista(lambda(x ls)
+    (if (null? ls)
+        (cons x '())
+        (miniLista-r x ls '())
+    )
+))
+
+(define agrupar-r(lambda (ls rtn prst)
+    (if (null? ls)
+        rtn
+        (if (elemntoPresente (car ls) prst)
+            (agrupar-r (cdr ls) rtn prst)
+            (agrupar-r (cdr ls) (cons (miniLista (car ls) ls) rtn) (cons (car ls) prst))
+        )
+    )
 ))
 
 (define agrupar (lambda (ls)
-                  (if (null? ls)
-                      '()
-                      (agrupar-r ls '() (car ls) (cons (car ls) '()))
-                      )
-                  ))
+    (if (null? ls)
+        '()
+        (agrupar-r ls '() '())
+    )
+))
+
+;;invierto primera, invierto segunda, construyo primera y segunda y despues invierto
+
+(define inverse(lambda (l1 rtn)
+    (if (null? l1)
+        rtn
+        (inverse (cdr l1) (cons (car l1) rtn))
+    )
+))
+
+
+(define concatenar (lambda (l1 l2)
+    (if (null? l1)
+        l2           
+        (let ((first1 (car l1)) (rest1 (cdr l1)))
+          (if (null? rest1)    
+              (cons first1 l2)   
+              (cons first1 (concatenar q l2)))))
+))
+
+(define primerNum(lambda (l1)
+    (if (null? l1)
+        0
+        (if (number? (car l1))
+            (car l1)
+            (let ((carl1 (car l1)))
+                (if (list? carl1)
+                    (primerNum carl1) ;; carl1 equals (car l1)
+                    (primerNum (cdr l1))
+                )    
+            )
+        )
+    )
+))
+
+(define aplanar (lambda ( ls )
+                   (if (empty? ls)
+                       ls
+                       (let ((fst (car ls)) (rst (cdr ls)))
+                         (if (list? fst)
+                             (concatenar (aplanar fst) (aplanar rst))
+                             (concatenar (cons fst '()) (aplanar rst))
+                             )))))
+
+(define attach-at-end (lambda (l1 l2)
+    (aplanar (list l2 l1))
+))
+
+(define boolToNumber(lambda (n)
+                       (if (equal? n "V")
+                           1
+                           0
+                       )
+))
+
+(define convertir-r(lambda (ls rtn)
+    (if (null? ls)
+        rtn
+        (let ((first (car ls)) (rest (cdr ls)))
+            (convertir-r (cdr rest) (cons (
+                (cond 
+                [(equal? first "T") (map string->number (car rest))]
+                [(equal? first "B") (map boolToNumber (car rest))]
+                [(equal? first "D") (map abs (car rest))]
+                [(display "NaN")]
+                )
+            ) rtn))
+        )
+    )
+))
+
+(define convertir(lambda (ls)
+    (convertir-r ls '())
+))
