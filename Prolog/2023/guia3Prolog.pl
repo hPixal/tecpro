e4192301ae101e290a92225a9f7e979bd3116389
hPixal <nahuel.j.gareis@protonmail.com>
2023-05-16 13:17:16 -0300

    guia3 wip

--- Prolog/2023/guia3Prolog.pl
+++ Prolog/2023/guia3Prolog.pl
@@ -23,13 +23,62 @@ agregar_nuevo(ELEM,[CAR|CDR],L):-
 
 semejanza([],[],0).
 semejanza([],[_|CDR],COUNT):- semejanza([],CDR,COUNTR), COUNT is COUNTR-1.
-semejanza([_|CDR],[],COUNT):- semejanza(CDR,[],COUNTR), COUNT is COUNTR-1.
+semejanza([_|CDR],[],COUNT):- semejanza(CDR,[],COUNTR), COUNT is COUNTR-1, !.
 
 semejanza([ELEM|CDR1],[ELEM|CDR2],COUNT):-
     semejanza(CDR1,CDR2,COUNTR),
-    COUNT is COUNTR + 1.
+    COUNT is COUNTR + 1, !.
 
 semejanza([CAR1|CDR1],[CAR2|CDR2],COUNT):-
     CAR1 \= CAR2,
     semejanza(CDR1,CDR2,COUNTR),
-    COUNT is COUNTR-1.
+    COUNT is COUNTR-1, !.
+
+%Ejercicio 6
+
+dic([sanar, hola, sabana, sabalo, prueba, computadora, cartera, mate, termo, mesa, silla, sarna]).
+
+encontrar(WORD,[WORD|_],[WORD]):- !.
+encontrar(_,[],[]).
+
+encontrar(WORD,[CAR|CDR],[[CAR,COUNT]|L2]):- 
+    atom_chars(CAR,X), atom_chars(WORD,Y),
+    semejanza(X,Y,COUNT),
+    COUNT > 0,
+    encontrar(WORD,CDR,L2), !.
+
+encontrar(WORD,[_|CDR],L):- 
+    encontrar(WORD,CDR,L).
+
+buscar(WORD,L):- dic(L2), encontrar(WORD,L2,L).
+
+%EJercicio 7 
+
+%reemplazar(elementoAReemplazar,reemplazo,numeroDeAparicion,cantidadDeVeces,Lista)
+reemplazar(_,_,_,[]).
+reemplazar(ELEM,REEM,NAP,CAV,[CAR|CDR]):- 
+    reemplazar(ELEM,REEM,NAP,CAV,CDR).
+
+reemplazar(ELEM,REEM,NAP,CAV,[CAR|CDR]):- %Reemplaza sin restriccion
+    ELEM == CAR,
+    CAV == -1,
+    NAP == 1,
+    reemplazar(ELEM,REEM,NAP,CAV,CDR2),
+    CDR = CDR2,
+    CAR is REEM.
+
+reemplazar(ELEM,REEM,NAP,_,[REM|CDR]):- %Caso pasa porque no le toca
+    NAP >= 1,
+    NAP2 is NAP-1,
+    reemplazar(ELEM,REEM,NAP2,CAV,CDR2),
+    CDR is CDR2.
+
+reemplazar(ELEM,REEM,NAP,CAV,[CAR|CDR]):- %Caso reemplaza pero tiene restriccion
+    CAR == ELEM,
+    CAV > 0,
+    NAP == 1,
+    CAR =/= ELEM,
+    CAV2 is CAV-1,
+    reemplazar(ELEM,REEM,NAP2,CAV2,CDR2),
+    CDR is CDR2.
+
